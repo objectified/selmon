e5569e14d145425f58d065db1df27dfbe06d9d83
objectified <objectified@gmail.com>
2014-12-07 21:55:52 +0100

    pep8 formatting

--- selmon/nagios/contextmanagers.py
+++ selmon/nagios/contextmanagers.py
@@ -3,6 +3,7 @@ from selmon.nagios.plugin import SelmonTestException
 from contextlib import contextmanager
 import time
 
+
 @contextmanager
 def benchmark(nagios_message, label, warning=3, critical=5):
     start_time = time.time()
@@ -15,10 +16,12 @@ def benchmark(nagios_message, label, warning=3, critical=5):
     )
 
     if elapsed > critical:
-        nagios_message.add_msg("'%s' exceeded critical threshold of %s" % (label, critical))
+        nagios_message.add_msg("'%s' exceeded critical threshold of %s" %
+                               (label, critical))
         nagios_message.raise_status(NagiosMessage.NAGIOS_STATUS_CRITICAL)
     elif elapsed > warning:
-        nagios_message.add_msg("'%s' exceeded warning threshold of %s" % (label, warning))
+        nagios_message.add_msg("'%s' exceeded warning threshold of %s" %
+                               (label, warning))
         nagios_message.raise_status(NagiosMessage.NAGIOS_STATUS_WARNING)
 
 
@@ -26,6 +29,6 @@ def benchmark(nagios_message, label, warning=3, critical=5):
 def test(nagios_message, label, status=NagiosMessage.NAGIOS_STATUS_CRITICAL):
     try:
         yield
-    except SelmonTestException as e:
+    except SelmonTestException:
         nagios_message.add_msg("Test failed: '%s'" % label)
         nagios_message.raise_status(status)
